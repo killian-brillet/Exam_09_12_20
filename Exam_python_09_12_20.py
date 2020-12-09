acda38b359fdf76b1d04b46b0f36c1798fc38d45
killian-brillet <brillet.k@gmail.com>
2020-12-09 15:15:52 +0100

    Update Exam_python_09_12_20.py

--- Exam_python_09_12_20.py
+++ Exam_python_09_12_20.py
@@ -4,33 +4,38 @@ from colorama import Fore, Back, Style
 import random
 
 def conditionvictoire (victoire):
-    if (motjoueur = motchoisi):
-        victoire = true
+    if (motjoueur == motchoisi):
+        victoire = 1
+
+def tableaumotrandom (motrandom):
+
+def tableaumotjoueur ():
 
 
 tableaumot = ["castor","cinema","citron","sapins","python","lettre","violet","marron","orange","avions"]
 motjoueur=[" "," "," "," "," "," "]
 motrandom=random.choice(tableaumot)
-motchoisi=[motrandom]
 
-victoire = false
+victoire = 0
 tour = 1
 
-print(motchoisi)
-print(mot)
+print(motrandom)
+print(motrandom[1])
 while (tour < 10):
+    motjoueur=input("Quel mot voulez vous essayer?")
     for i in range (0,6):
-        if motjoueur[i] = motchoisi[i]:
+        if (motjoueur[i] == motchoisi[i]):
             print (Back.RED + motjoueur[i], end=" ")
-        if motjoueur[i] !=  motchoisi[i]:
-            print (Back.BLUE + motjoueur[i], end" ")
+        if (motjoueur[i] !=  motchoisi[i]):
+            print (Back.BLUE + motjoueur[i], end=" ")
+    print (motjoueur)
     tour = tour + 1
     conditionvictoire (victoire)
-    if (victoire = true):
+    if (victoire == 1):
         print("Bravo vous avez gagné")
         input()
 
-if (victoire = false):
+if (victoire == 0):
     print("Vous avez perdu, le mot etait ", motchoisi)
     
 
