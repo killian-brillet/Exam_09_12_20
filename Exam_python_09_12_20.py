f929ccc96778f2ba95dfc592cd0b7950cd26112d
killian-brillet <brillet.k@gmail.com>
2020-12-09 17:35:30 +0100

    Update Exam_python_09_12_20.py

--- Exam_python_09_12_20.py
+++ Exam_python_09_12_20.py
@@ -1,10 +1,12 @@
 #import colorama et random
+
 from colorama import init 
 init() 
 from colorama import Fore, Back, Style 
 import random
 
 #fonctions    
+
 def couleurmot (motjoueur, motrandom):
     for i in range (len(motjoueur)):
         if (motjoueur[i] == motrandom[i]):
@@ -17,18 +19,21 @@ def couleurmot (motjoueur, motrandom):
     return motjoueur, motrandom
 
 #variables
+
 tableaumot = ["castor","toucan","chaise","sapins","python","lettre","violet","marron","orange","cactus"]
 motrandom=random.choice(tableaumot)
 victoire = False
 tour = 1
 
 #texte intro
+
 print (" Bienvenue à Motus! Vous devez deviner le mot en enoncant d'autres mots.")
 print ("   Si la lettre s'affiche en rouge, elle est à la bonne place")
 #print ("   Si elle s'affiche en jaune, elle n'est pas à la bonne place")
 print ("   Si elle s'affiche en bleu, elle n'est meme pas dans le mot \n Bon jeu!")
 
 #programme
+    
 while (tour <= 8 and victoire != True):
     motjoueur=str(input("Quel mot voulez vous essayer? \n"))
     if (len(motjoueur) == 6):
@@ -41,6 +46,7 @@ while (tour <= 8 and victoire != True):
         print(" \nVeuillez rentrer un mot à 6 lettres \n")
 
 #Fin de partie
+
 if (victoire == True):
     print("Vous avez gagné, le mot etait bien", Back.RED + motrandom)
 
