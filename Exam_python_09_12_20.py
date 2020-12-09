aefe0119cbeb5c867c56f665da3c487b79410a17
killian-brillet <brillet.k@gmail.com>
2020-12-09 17:02:46 +0100

    Update Exam_python_09_12_20.py

--- Exam_python_09_12_20.py
+++ Exam_python_09_12_20.py
@@ -1,29 +1,35 @@
+#import colorama et random
 from colorama import init 
 init() 
 from colorama import Fore, Back, Style 
 import random
-    
+
+#fonctions    
 def couleurmot (motjoueur, motrandom):
     for i in range (len(motjoueur)):
         if (motjoueur[i] == motrandom[i]):
             print (Back.RED + motjoueur[i], end=" ")
-        if (motjoueur[i] !=  motrandom[i]):
+        #if (motjoueur[i] != motrandom[i] and motjoueur[i] == motrandom[1] or motjoueur[i] == motrandom[2] or motjoueur[i] == motrandom[3] or motjoueur[i] == motrandom[4] or motjoueur[i] == motrandom[5]):
+        #    if (motjoueur[i] != motrandom[i]):
+        #        print (Back.YELLOW + motjoueur[i], end=" ")
+        if (motjoueur[i] != motrandom[i]):
             print (Back.BLUE + motjoueur[i], end=" ")
-        #if (motjoueur[i] != motrandom[i] and ):
-        #    print (Back.YELLOW + motjoueur[i], end=" ")
         print (Style.RESET_ALL)
     return motjoueur, motrandom
 
-tableaumot = ["castor","cinema","citron","sapins","python","lettre","violet","marron","orange","avions"]
+#variables
+tableaumot = ["castor","cinema","citron","sapins","python","lettre","violet","marron","orange","cactus"]
 motrandom=random.choice(tableaumot)
 victoire = False
 tour = 1
 
-print (" Bienvenue à Motus! vous devez deviner le mot en enoncant d'autres mots.")
+#texte intro
+print (" Bienvenue à Motus! Vous devez deviner le mot en enoncant d'autres mots.")
 print ("   Si la lettre s'affiche en rouge, elle est à la bonne place")
-print ("   Si elle s'affiche en jaune, elle n'est pas à la bonne place")
+#print ("   Si elle s'affiche en jaune, elle n'est pas à la bonne place")
 print ("   Si elle s'affiche en bleu, elle n'est meme pas dans le mot \n Bon jeu!")
 
+#programme
 while (tour <= 8 and victoire != True):
     motjoueur=str(input("Quel mot voulez vous essayer? \n"))
     if (len(motjoueur) == 6):
@@ -34,6 +40,7 @@ while (tour <= 8 and victoire != True):
     else:
         print(" \nVeuillez rentrer un mot à 6 lettres \n")
 
+#Fin de partie
 if (victoire == True):
     print("Vous avez gagné, le mot etait bien", Back.RED + motrandom)
 
